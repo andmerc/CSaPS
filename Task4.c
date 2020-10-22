f196c76b77f582d00a186e355dc23737074377e5
andmerc <32245832+andmerc@users.noreply.github.com>
2020-10-22 13:21:47 +0300

    Update and rename Task4.sh to Task4.c

---
+++ Task4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Не хватает аргументов. Использование: program 'filename'");
+    }
+    else
+    {
+        char* filename = argv[1];
+        int descriptor = open(filename, O_RDONLY);
+    
+        if (descriptor != -1) {
+            FILE *file = fdopen(descriptor, "rb");
+    
+            if (file) {
+                struct stat statistics;
+    
+                if (fstat(descriptor, &statistics) != -1) {
+                    printf("Размер файла %s: %ld", filename, statistics.st_size);
+                }
+                fclose(file);
+            }
+            close(descriptor);
+        }
+    }
+
+    return 0;
+}
--- Task4.sh
+++
@@ -1,6 +0,0 @@
-filename=$1
-if [ -z $filename ]; then
-	echo 'Наименование файла отсутствует'
-else
-	echo 'Размер файла '$filename': '$(stat --format=%s $filename)
-fi
